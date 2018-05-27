1ebd7c02dcd5e69c39f0977fe57488289ccda02b
Jorge Rama Krsna Mandoju <jorgezin@gmail.com>
2018-05-27 01:48:27 -0300

    inserção do erro na requisição do service

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -1,28 +1,24 @@
+import {Injectable} from '@angular/core'
+import {Http} from '@angular/http'
+
+import {Observable} from 'rxjs/Observable'
+import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+
+
 import { Restaurant } from "app/restaurants/restaurant/restaurant.model";
 
+import {MEAT_API} from 'app.api'
+import { ErrorHandler} from 'app/app.error-handler';
+
+@Injectable()
 export class RestaurantsService{
-    constructor(){}
-
-    rests: Restaurant[] = [
-        {
-          id: "bread-bakery",
-          name: "Bread & Bakery",
-          category: "Bakery",
-          deliveryEstimate: "25m",
-          rating: 4.9,
-          imagePath: "assets/img/restaurants/breadbakery.png",
-        },
-        {
-          id: "burger-house",
-          name: "Burger House",
-          category: "Hamburgers",
-          deliveryEstimate: "100m",
-          rating: 3.5,
-          imagePath: "assets/img/restaurants/burgerhouse.png",
     
-        }]
+    constructor(private http: Http){}
 
-    restaurants(): Restaurant[] {
-        return this.rests
+    restaurants(): Observable<Restaurant[]> {
+        return this.http.get(`${MEAT_API}/restaurants`)
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError)
     }
 }
